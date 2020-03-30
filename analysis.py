54df763311531c1999ee2122f5778619f5b662c0
jbrad88 <jodybradley@gmail.com>
2020-03-30 18:58:52 +0100

    New approach

--- analysis.py
+++ analysis.py
@@ -1,19 +1,7 @@
-# Jody Bradley
+import pandas as pd
+from pandas import read_csv
+import numpy as np 
 
-import pandas as pd 
-import numpy as np
+data = pd.read_csv('iris.csv')
 
-# Imports convenient data structure for dealing with numerical data.
-
-data = pd.read_csv("iris.csv")
-# Opens file iris.csv
-
-sepal.length = 
-
-
-
-
-# REFERENCES
-# 1. https://www.datacamp.com/community/tutorials/pandas-read-csv
-
-
+print(data.describe())
--- iris3.py
+++ iris3.py
@@ -33,5 +33,7 @@ print(dataset.describe())
 
 
 
+
+
 # REFERENCES
 # 1. https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
