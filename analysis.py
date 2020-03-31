fb3b3ccd6681ba33127d385f93600657b4993e4a
jbrad88 <jodybradley@gmail.com>
2020-03-31 12:07:58 +0100

    Cleaning up code. Save as CSV file

--- analysis.py
+++ analysis.py
@@ -10,21 +10,16 @@ data = pd.read_csv('iris.csv')
 
 summary = data.describe()
 
-# file1 = open(r"Iris-data.txt", "a")
-# Creates and opens a text file titled Iris-data.txt [3]
+file1 = open(r"Iris-data.csv", "a")
+# Creates and opens a CSV file titled Iris-data [3]
 
-# np.savetxt(r'Iris-data.txt', summary.values, fmt='%d')
-# Saves summary to text file Iris-data.txt. However, there are no headings [4].
+summary.to_csv(r'Iris-data.csv')
+# Saves summary to a CSV file[5]
 
-summary.to_csv(r'Iris-data.txt')
-# Saves summary to CSV file. Headings and indices now included.
-
-
+file1.close()
+# Closes CSV file.
 
 
-
-# file1.close()
-# Closes text file Iris-data.txt
 
 
 
@@ -33,6 +28,7 @@ summary.to_csv(r'Iris-data.txt')
 # 1. https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 # 2.https://www.datacamp.com/community/tutorials/pandas-read-csv
 # 3. https://www.geeksforgeeks.org/reading-writing-text-files-python/
-#4. https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
+# 4. https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
+# 5. https://datatofish.com/export-dataframe-to-csv/
 
 
