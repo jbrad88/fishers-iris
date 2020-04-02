cc3b453a4c1faa30bdce0cdc606e941ede46e4eb
jbrad88 <jodybradley@gmail.com>
2020-04-02 14:04:06 +0100

    1st plotting attempt

--- analysis.py
+++ analysis.py
@@ -5,16 +5,33 @@
 import pandas as pd
 from pandas import read_csv
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('iris.csv')
 
-summary = data.describe()
+# The first part of this program will output a summary of each variable to a single text file (in CSV format).
+# This will give the user the mean, the standard variation, the minimum, the maximum of each of the four attributes
+# (sepal length, sepal width, petal length and petal width).
 
-file1 = open(r"Iris-data.csv", "a")
+df = pd.read_csv('iris.csv')
+# Loans data from the CSV file 'iris.csv' into pandas dataframe.
+
+summary = df.describe()
+
+file1 = open(r"Iris-data.csv", 'a')
 # Creates and opens a CSV file titled Iris-data [3]
 
 summary.to_csv(r'Iris-data.csv')
 # Saves summary to a CSV file[5]
+
+plt.plot(df['sepal_length'], df['sepal_width'], 'bo') 
+plt.title("sepal length vs sepal width")
+plt.show()
+
+#plt.savefig("scatter.png")
+#plt.clf()
+
+#df.hist(figsize=(10,5))
+#plt.show()
 
 file1.close()
 # Closes CSV file.
@@ -26,7 +43,7 @@ file1.close()
 
 # REFERENCES
 # 1. https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
-# 2.https://www.datacamp.com/community/tutorials/pandas-read-csv
+# 2. https://www.datacamp.com/community/tutorials/pandas-read-csv
 # 3. https://www.geeksforgeeks.org/reading-writing-text-files-python/
 # 4. https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
 # 5. https://datatofish.com/export-dataframe-to-csv/
---
+++ ipython_log.py
@@ -0,0 +1,30 @@
+# IPython log file
+
+get_ipython().run_line_magic('logstart', '')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+pd.read_csv('Iris-data.csv')
+pd.read_csv('Iris.csv')
+df['sepal_length']
+df[''sepal length'']
+df[''sepal_length'']
+df = pd.read_csv('Iris.csv')
+df
+df['sepal_length']
+plt.plot(df['sepal_length'])
+plt.plot(df['sepal_length'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'])
+plt.hist(df['sepal_length'], df['sepal_width'])
+df
+(df['sepal_length'], df['sepal_width'], 'b')
+df
+plt.plot(df['sepal_length'], df['sepal_width'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'], 'bo')
+plt.plot(df['sepal_length'], df['sepal_width'], 'bo')
+plt.plot(df['sepal_length'], df['sepal_width'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'], 'b')
+plt.plot(df['sepal_length'], df['sepal_width'], 'bo')
+plt.plot(df['sepal_length'], df['sepal_width'], 'bo')
