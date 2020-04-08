47316234fad9af7fae96e348920f7375c60449b9
jbrad88 <jodybradley@gmail.com>
2020-04-08 17:01:17 +0100

    Adding program for basic scatter plot

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ import pandas as pd
 from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 # The first part of this program will output a summary of each variable to a single text file (in CSV format).
 # This will give the user the mean, the standard variation, the minimum, the maximum of each of the four attributes
@@ -23,7 +23,9 @@ file1 = open(r"Iris-data.csv", 'a')
 summary.to_csv(r'Iris-data.csv')
 # Saves summary to a CSV file[5]
 
+# This next part of the program will save a histrogram as a PNG for each of the four variables.
 plt.hist(df["sepal_length"])
+plt.title("Sepal Length")
 plt.savefig("sepal_length.png")
 plt.clf()
 # Saves histrogram for Sepal Length as PNG
@@ -43,14 +45,18 @@ plt.savefig("petal_width.png")
 plt.clf()
 # Saves histrogram for petal width as PNG
 
-#plt.plot(df['sepal_length'], df['sepal_width'], 'bo') 
-#plt.title("sepal length vs sepal width")
-#plt.show()
+# This next part of the program will save a scatter plot of each pair of variables: (i) sepal length to sepal width;
+# and (ii) petal length to petal width.
 
-#plt.savefig("scatter.png")
-#plt.clf()
+plt.plot(df["sepal_length"], df["sepal_width"], 'b,') 
+plt.title("Sepal Length vs Sepal Width")
+plt.savefig("sepal_scatter.png")
+plt.clf()
 
-#df.hist(figsize=(10,5))
+plt.plot(df["petal_length"], df["petal_width"], 'b,') 
+plt.title("Petal Length vs Petal Width")
+plt.savefig("petal_scatter.png")
+plt.clf()
 
 
 file1.close()
@@ -67,5 +73,6 @@ file1.close()
 # 3. https://www.geeksforgeeks.org/reading-writing-text-files-python/
 # 4. https://stackoverflow.com/questions/31247198/python-pandas-write-content-of-dataframe-into-text-file
 # 5. https://datatofish.com/export-dataframe-to-csv/
+# 6. https://www.kaggle.com/gopaltirupur/iris-data-analysis-and-machine-learning-python
 
 
